d5e2ddb40277ba86fdab1bba43b83480d937ff75
ishankj23 <kaustubhij23@gmail.com>
2024-11-27 18:24:44 +0530

    transformation complete

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,7 +16,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
@@ -65,7 +65,7 @@ class DataTransformation:
             preprocessor=ColumnTransformer(
                 [
                 ("num_pipeline",num_pipeline,numerical_columns),
-                ("cat_pipelines",cat_pipeline,categorical_columns)
+                ("cat_pipeline",cat_pipeline,categorical_columns)
 
                 ]
 
@@ -106,9 +106,9 @@ class DataTransformation:
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[
-                input_feature_train_arr, np.array(target_feature_train_df)
+                input_feature_train_arr, np.array(target_feature_train_df).reshape(1,-1)
             ]
-            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df).reshape(1,-1)]
 
             logging.info(f"Saved preprocessing object.")
 
